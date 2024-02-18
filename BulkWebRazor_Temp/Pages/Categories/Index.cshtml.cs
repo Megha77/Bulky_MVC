dac73d3843da306e0dec0d60ce7e122f27639056
Megha <meghakurwade7@gmail.com>
2024-02-18 21:09:54 +0530

    Razor Project- Category table added

---
+++ BulkWebRazor_Temp/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkWebRazor_Temp.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
